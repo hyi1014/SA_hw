4f05d5c831baf594fd6133bd9dde4754b6b66da8
hyi1014 <hyi28915088@gmail.com>
2023-03-16 00:49:24 +0800

    hash checksum v1.0.1

---
+++ sahw2.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#help function
+function usage() 
+{
+    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...
+    \n--sha256: SHA256 hashes to validate input files.\n--md5:MD5 hashes to validate input files.\n-i: Input files.\n"
+    exit 0
+}
+#error
+function err_args() 
+{
+    echo -n -e "Error: Invalid arguments.\n" 1>&2
+    usage
+    exit 1
+}
+
+function err_vals() 
+{
+    echo -n -e "Error: Invalid values.\n" 1>&2
+    exit 1
+}
+
+function err_csum()
+{
+    echo -n -e "Error: Invalid checksum.\n" 1>&2
+    exit 1
+}
+
+function err_type()
+{
+    echo -n -e "Error: Only one type of hash function is allowed.\n" 1>&2
+    exit 1
+}
+#check hash value 
+function check_hash()
+{
+    pos_i=0
+    pos_h=0
+   #get the position of -i and -hash
+    for ((i=1 ; i<$# ; i++))
+    do
+        [[ "${!i}" = "-i" ]] && pos_i=${i}
+        if [[ "${!i}" = "--md5" || "${!i}" = "--sha256" ]]
+        then
+            [ $pos_h != 0 ] && err_type
+            pos_h=${i}
+        fi
+    done
+    #check len_1 = len_2
+    (( "${pos_i}" >= "${pos_h}" )) &&  len_1=$((${pos_i}-${pos_h}-1))
+    (( "${pos_i}" < "${pos_h}" )) &&  len_1=$((${pos_h}-${pos_i}-1))
+    len_2=$((${#@}-$len_1-2))
+    # echo "pos_i: ${pos_i}"
+    # echo "pos_h: ${pos_h}"
+    # echo "len_1: ${len_1}"
+    # echo "len_2: ${len_2}"
+    if [ $len_1 != $len_2 ]
+    then
+        err_vals
+    fi
+
+    #check hash value
+    buffer_file=${@:$((pos_i+1)):${len_1}}
+    buffer_hash=${@:$((pos_h+1)):${len_1}}
+    if [ "${!pos_h}" = "--md5" ]
+    then
+        for ((i=0 ; i<${len_1} ; i++))
+        do
+            if [ "${buffer_hash[i]}" != "$(md5sum ${buffer_file[i]} | awk '{print $1}')" ]
+            then
+                err_csum
+            fi
+        done
+    elif [ "${!pos_h}" = "--sha256" ]
+    then
+        for ((i=0 ; i<${len_1} ; i++))
+        do
+            if [ "${buffer_hash[i]}" != "$(sha256sum ${buffer_file[i]} | awk '{print $1}')" ]
+            then
+                err_csum
+            fi
+        done
+    else
+        err_vals
+    fi
+    exit 0
+}
+
+if [ "$1" = "-h" ]
+then
+    usage
+elif [ "$1" = "--md5" ] || [ "$1" = "--sha256" ] || [ "$1" = "-i" ]
+then
+    check_hash $@
+else
+    err_args 
+fi
+
